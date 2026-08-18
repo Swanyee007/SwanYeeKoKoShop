50ea8f294372e4686fa55529339322dcb3abd482
Swan Yee <swanyeekhine@gmail.com>
2026-08-18 06:34:00 +0630

    sixth

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -8,7 +8,7 @@ class FrontController extends Controller
 {
     public function shop()
     {
-        $items=Item::all();
+        $items=Item::orderBy('id','DESC')->paginate(8);
         //var_dump($items);
         return view('front.shop',compact('items'));
     }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::get('/',[App\Http\Controllers\FrontController::class,'shop'])->name('shop
 Route::get('/shop-item/{id}',[App\Http\Controllers\FrontController::class,'shopItem'])->name('shop-item');
 Route::group(['prefix'=>'backend','as'=>'backend.'],function(){
     Route::get('/',[App\Http\Controllers\Admin\DashboardController::class,'index'])->name('dashboard');
+    Route::resource('items',App\Http\Controllers\Admin\ItemController::class);
 
 });
 Auth::routes();
